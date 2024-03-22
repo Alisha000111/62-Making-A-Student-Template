0af0ae4ddd0a5942d0328353c5e751786cce02cb
Alisha M Irfan <160208713+Alisha000111@users.noreply.github.com>
2024-03-22 13:56:07 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+// Filling in the blueprint with an example student
+var student = {
+    name: "Alisha",
+    age: 20,
+    courses: ["Chemistry", "Biology", "Civics"]
+};
+// Showing the student's information
+console.log(student);
+// We're using the blueprint to make sure our student has a name, age, and list of courses.
